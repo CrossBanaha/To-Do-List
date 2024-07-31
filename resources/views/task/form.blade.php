c8ab51c038b0d5d0ed08c04c8034da08c72eb543
CrossBanaha <stephano.carbajal@ucsm.edu.pe>
2024-07-31 13:10:58 -0500

    form.blade.php: The code is adapted so that it can work together with the insert.blade.php view.

--- resources/views/task/form.blade.php
+++ resources/views/task/form.blade.php
@@ -1,18 +1,7 @@
-<div class="container">
-    <form action="{{route('task.create')}}" method="post">
-        @csrf
-        <input type="text" placeholder="Enter Task" name="task" id="task" value="{{ old('task') }}" autofocus>
-        @error('task')
-            <div class="text-red-500 border-red-600">{{ $message }}</div>
-        @enderror
-        <input type="text" placeholder="Enter Description" name="description" id="description" value="{{ old('description') }}" autofocus>
-        @error('description')
-            <div class="text-red-500 border-red-600">{{ $message }}</div>
-        @enderror
-        <button id="add" class="addButton">
-            ADD
-            <nav></nav>
-            <nav></nav>
-        </button>
-    </form>
+<div class="w-[450px] flex p-5 m-auto justify-center">
+    <button id="add" class="addButton mx-auto" onclick="openModal()">
+        ADD
+        <nav></nav>
+        <nav></nav>
+    </button>
 </div>
