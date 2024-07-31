729b0135bff4d04ca1f3f92ad291d302c533a32a
CrossBanaha <stephano.carbajal@ucsm.edu.pe>
2024-07-31 13:15:02 -0500

    web.php: Calls from index.blade.php to TaskController.php are modified.

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-/* 
-Route::get('/', function () {
-    return view('task.index');
-});
-*/
-//Route::resource('task','TaskController');
 Route::get('/',[TaskController::class,'index']);
-Route::post('/', [TaskController::class,'store'])->name('task.create');
-Route::put('/{id}', [TaskController::class,'update'])->name('task.update');
+Route::post('/', [TaskController::class,'store'])->name('task.store');
+Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update');
 Route::patch('/{id}', [TaskController::class, 'toggleStatus'])->name('task.toggleStatus');
-Route::delete('/{id}', [TaskController::class,'destroy'])->name('task.destroy');
+Route::delete('/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
