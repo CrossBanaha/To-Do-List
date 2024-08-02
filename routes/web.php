6fe6fffda73654c232cdea87dd757c964bae9996
CrossBanaha <stephano.carbajal@ucsm.edu.pe>
2024-08-01 19:08:45 -0500

    It was possible to implement Route::resource(), using an exception in the methods not used and at the same time, maintaining the custom route.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-Route::get('/',[TaskController::class,'index']);
-Route::post('/', [TaskController::class,'store'])->name('task.store');
-Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update');
-Route::patch('/{id}', [TaskController::class, 'toggleStatus'])->name('task.toggleStatus');
-Route::delete('/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+Route::resource('tasks', TaskController::class)->except(['create','show','edit']);
+Route::patch('/{task}', [TaskController::class, 'toggleStatus'])->name('tasks.toggleStatus');
+//Route::get('/',[TaskController::class,'index']);
+//Route::post('/', [TaskController::class,'store'])->name('task.store');
+//Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update');
+//Route::delete('/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
